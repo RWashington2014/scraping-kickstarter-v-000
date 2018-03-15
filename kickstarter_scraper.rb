f3501dda30096f707bf1e22765ff4f391f29aff9
Robert W <robwash@gmail.com>
2018-03-15 03:09:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
   # title: projects.css("h2.bbcard_name strong a").text
+  # image_link: project.css("div.project-thumbnail a img").attribute("src").value
+  
 
   binding.pry
 end
